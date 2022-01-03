7d3f95436a4fd4b6f009e6007add342d0594e58b
Евгений Нестеров <e.y.nesterov@gmail.com>
2022-01-03 21:02:45 +0300

    Модули

---
+++ MetaWeather/Models/WheatherLocation.cs
@@ -0,0 +1,24 @@
+﻿
+using MetaWeather.Service;
+
+using System.Text.Json.Serialization;
+
+namespace MetaWeather.Models
+{
+    public class WheatherLocation
+    {
+        [JsonPropertyName("woeid")]
+        public int Id { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        [JsonPropertyName("location_type")]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public LocationType Type { get; set; }
+        [JsonPropertyName("latt_long")]
+        [JsonConverter(typeof(JsonCoordinateConverter))]
+        public (double Latitude, double Longitude) Location { get; set; }
+        [JsonPropertyName("distance")]
+        public int Distance { get; set; }
+
+    }
+}

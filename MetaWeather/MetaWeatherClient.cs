d4f866b9b96d42e4eb06e3f642227eeb30fe4d6a
Евгений Нестеров <e.y.nesterov@gmail.com>
2022-01-03 21:04:11 +0300

    HTTP клиент и первый метод получающий локацию по названию

---
+++ MetaWeather/MetaWeatherClient.cs
@@ -0,0 +1,35 @@
+﻿using MetaWeather.Models;
+
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MetaWeather
+{
+    public class MetaWeatherClient
+    {
+        private readonly HttpClient _client;
+
+        public MetaWeatherClient(HttpClient Client) => _client = Client;
+        //private static readonly JsonSerializerOptions __jsonOptions = new()
+        //{
+        //    Converters =
+        //    {
+        //        new JsonStringEnumConverter() ,
+        //        new    JsonCoordinateConverter(),
+
+        //    }
+        //};
+
+        public async Task<WheatherLocation[]> GetLocationByName(string name, CancellationToken cancel = default)
+        {
+            return await _client
+                .GetFromJsonAsync<WheatherLocation[]>($"api/location/search/?query={name}"/*, __jsonOptions*/, cancel)
+                .ConfigureAwait(false);
+        }
+
+    }
+}

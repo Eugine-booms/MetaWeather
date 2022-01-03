e020706db24064c42666a0772e5f9018b7d257eb
Евгений Нестеров <e.y.nesterov@gmail.com>
2022-01-03 20:03:36 +0300

    Настройка тестового настольного проекта

--- Tests/MetaWeather.TestConsole/Program.cs
+++ Tests/MetaWeather.TestConsole/Program.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using System;
+using System.Threading.Tasks;
 
 namespace MetaWeather.TestConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IHost __hosting;
+        public static IHost Hosting => __hosting ??= CheateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+        private static IHostBuilder CheateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServises);
+
+        private static void ConfigureServises(HostBuilderContext host, IServiceCollection services)
         {
+
+        }
+
+        static async Task Main(string[] args)
+        {
+            using var host = Hosting;
+            await host.StartAsync();
+            Console.WriteLine("Завершено");
+            Console.ReadLine();
+            await host.StopAsync();
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
